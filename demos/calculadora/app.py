5936f40498281b5fbd54983a860a65cfebb3dac3
YohrggenOlaf <1725110905@utectulancingo.edu.mx>
2026-06-11 02:35:56 +0000

    UPDATE calculadora1

--- demos/calculadora/app.py
+++ demos/calculadora/app.py
@@ -20,9 +20,18 @@ class Calculadora:
     
     def POST(self):
         formulario = web.input()
-        numero_1 = formulario['prinum']
-        numero_2 = formulario['segnum']
-        resultado = float(numero_1) + float(numero_2)
+        numero_1 = float(formulario['prinum'])
+        numero_2 = float(formulario['segnum'])
+        operacion = formulario.get('operacion')
+        match operacion:
+            case "suma": resultado = numero_1 + numero_2 
+            case "resta": resultado = numero_1 - numero_2
+            case "multiplica": resultado = numero_1 * numero_2
+            case "divide": resultado = numero_1 / numero_2
+            case "modulo": resultado = numero_1 % numero_2
+            case "potencia": resultado = numero_1 ** numero_2
+            case "raiz": resultado = numero_1 ** 0.5
+
         return render.calculadora(numero_1, numero_2, resultado)
 
 if __name__ == "__main__":
